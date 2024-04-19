8f60de4a50397606125df11af7d04a2b0e8e64e3
SEC-Tito <mateoenclada52@gmail.com>
2024-04-19 12:56:40 -0500

    19-04-2024

--- src/app/audio/audio.page.ts
+++ src/app/audio/audio.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Database, object, ref, set } from '@angular/fire/database';
+import { AlertController } from '@ionic/angular';
+import { onValue } from 'firebase/database';
 
 @Component({
   selector: 'app-audio',
@@ -7,19 +10,70 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 })
 export class AudioPage implements OnInit {
   @ViewChild('audioElement') audioElement: any;
-  constructor() { }
+  constructor(private database: Database, private alertController: AlertController) { }
 
   ngOnInit() {
+    const estadoARef = ref(this.database, '/Respuestas Audio/EstadoA');
+
+    onValue(estadoARef, (snapshot) => {
+      const estadoA = snapshot.val();
+      console.log('EstadoA actual:', estadoA);
+    });
   }
-  stopAll(){
+  stopAll() {
     const audio = this.audioElement.nativeElement as HTMLAudioElement;
     audio.pause();
   }
+
   playAudio() {
     const audio = this.audioElement.nativeElement as HTMLAudioElement;
     audio.src = 'assets/audio/juaquin.mp3';
     audio.load();
     audio.play();
-    console.log(`Reproduciendo: ${audio.src}`);
+    console.log({audio});
+    
   }
+
+  sendResponse() {
+    const estadoAudio = this.leerCheckbox();
+
+    const estadoA = estadoAudio ? 1 : 0;
+
+    const estadoARef = ref(this.database, '/RespuestaA/EstadoA');
+    set(estadoARef, estadoA);
+    if (estadoAudio) {
+      this.presentAlert('¡Sus respuestas son correctas!');
+    } else {
+      this.presentAlert('¡Sus respuestas son incorrectas!');
+    }
+  }
+
+  leerCheckbox(): boolean {
+    const checkboxes = document.querySelectorAll('.custom-checkbox-true');
+    let checkedCount = 0;
+    let customCheckedCount = 0;
+
+    checkboxes.forEach((checkbox: any) => {
+      if (checkbox.checked) {
+        checkedCount++;
+      }
+      customCheckedCount++;
+    });
+
+    if (checkedCount === customCheckedCount && checkedCount > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  async presentAlert(message: string) {
+    const alert = await this.alertController.create({
+      header: '¡Alerta!',
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
