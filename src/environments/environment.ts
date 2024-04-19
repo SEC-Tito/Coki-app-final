76a011da8011775de447a8884f02194f8dcda47f
SEC-Tito <mateoenclada52@gmail.com>
2024-04-19 13:29:20 -0500

    carajo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,16 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
+import { initializeApp } from "firebase/app";
 
 export const environment = {
   production: false,
   firebase: {
-  apiKey: "AIzaSyAPglCsqN_UC8kqhf_XUZ1h7U1AFEhfMP8",
-  authDomain: "coki-proyect-d511b.firebaseapp.com",
-  projectId: "coki-proyect-d511b",
-  storageBucket: "coki-proyect-d511b.appspot.com",
-  messagingSenderId: "537749263498",
-  appId: "1:537749263498:web:49a26ea0235815c7af0973"
-}
+    apiKey: "AIzaSyAPglCsqN_UC8kqhf_XUZ1h7U1AFEhfMP8",
+    authDomain: "coki-proyect-d511b.firebaseapp.com",
+    databaseURL: "https://coki-proyect-d511b-default-rtdb.firebaseio.com",
+    projectId: "coki-proyect-d511b",
+    storageBucket: "coki-proyect-d511b.appspot.com",
+    messagingSenderId: "537749263498",
+    appId: "1:537749263498:web:49a26ea0235815c7af0973"
+  }
 };
 
 /*
